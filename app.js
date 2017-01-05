7ed6da5730eafb039a86cb0df18658137389f645
David <david.diez31@free.fr>
2017-01-05 11:31:33 +0100

    exercice ok

--- app.js
+++ app.js
@@ -9,29 +9,18 @@ function main() {
     tout votre code doit se trouver dans cette fonction,
     vous pouvez biensur créer d'autres fonctions si nécessaire
     */
-
     // 1. placez un listener sur le click des éléments <button>
+    // 2. dans le callback récupérez le data attribute animal
+    // 3. récupérerez dans l'objet species la valeur correspond à l'attribut récupéré
+    // 4. ajoutez élément img en assignant à son attibut src la valeur précédemment récupérée
+    // 5. inserez cet élément dans l'élément ayant l'id holder
+    /* $("#holder").before('<img src="img/bear.jpg" alt="description">'); */
+    // 
+    // Conseils : avancez étape par étape, console.log et debugger autant que nécessaires !
+
     $(".button").click(function() {
-
-        // 2. dans le callback récupérez le data attribute animal
         var animal = $(this).attr("data-animal");
-
-        // 3. récupérerez dans l'objet species la valeur correspond à l'attribut récupéré
-        var animalSelect = $(species.val);
-        console.log(animalSelect);
-
-        // 4. ajoutez élément img en assignant à son attibut src la valeur précédemment récupérée
-        var img = $("img").attr({ src: "species" });
-
-
-        // 5. inserez cet élément dans l'élément ayant l'id holder
-        /* $("#holder").before('<img src="img/bear.jpg" alt="description">'); */
-
-        // 
-        // Conseils : avancez étape par étape, console.log et debugger autant que nécessaires !
-
-
-
+        $("#holder").html("<img src='img/" + species[animal] + "'/>");
     })
 }
 
